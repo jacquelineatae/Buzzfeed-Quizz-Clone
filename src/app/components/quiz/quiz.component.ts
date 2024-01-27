878ea3c2661f5337d4bdbb78be52481a901842ab
jacquelineatae <dev.atae02@gmail.com>
2024-01-27 20:04:38 -0300

    Creating logic properties

--- src/app/components/quiz/quiz.component.ts
+++ src/app/components/quiz/quiz.component.ts
@@ -1,12 +1,30 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-quiz',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './quiz.component.html',
   styleUrl: './quiz.component.css'
 })
 export class QuizComponent {
+  title: string = ""
+
+  questions: any
+  questionSelected: any
+
+  answers:string[] = []
+  answerSelected:string = ""
+
+  questionIndex:number = 0
+  questionMaxIndex:number = 0
+
+  finished:boolean = false
+
+  constructor(){}
+
+  ngOnInit(): void {
+  }
 
 }
